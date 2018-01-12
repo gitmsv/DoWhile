c2c6841f30828a7f9e7f09a9f3293378632b8e33
$.m.lnk <smjvlab@gmail.com>
2018-01-12 23:03:25 +0200

    This class shows the work of the do-while

---
+++ src/dowhile/DoWhile.java
@@ -0,0 +1,12 @@
+package dowhile;
+/*This class shows the work of the do-while*/
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        do {
+            System.out.println("такт " + n);
+            n--;
+        } while (n >= 0);
+    }
+
+}
